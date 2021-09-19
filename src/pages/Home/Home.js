9c8fb093feb2653532cd5af76d78870cb3a150b9
Felipe de Paula <emilcyfelipe@gmail.com>
2021-09-19 15:33:23 -0300

    logout and user data

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,45 +1,42 @@
 import React, { useEffect, useState } from "react";
-import {getAuth, onAuthStateChanged } from "firebase/auth"
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useHistory } from "react-router";
 
+const Home = () => {
+  const history = useHistory();
+  const auth = getAuth();
+  const [userEmail, setUserEmail] = useState(null);
 
-const Home = ()=>{
-    const history = useHistory();
-    const auth = getAuth();
-    const [cUser,setCUser] = useState(auth.currentUser);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user !== null) {
+        setUserEmail(user.email);
+      } else {
+        history.push("/signin");
+      }
+    });
+  }, [auth, history]);
 
-    useEffect(()=>{
-        onAuthStateChanged(auth,(user) =>{
-            if(!user){
-                history.push('/signin')
-                
-            }else{
-               
-                setCUser(user.email)
-            }
-        } 
-    )
-    },[auth,history])
+  function logout() {
+    auth.signOut();
+  }
 
-
-    function logout(){
-        auth.signOut();
-    }
-
-    
-    return(
-        <>
+  return (
+    <>
+      <div>
+        <h2>Bem vindo</h2>
+        {auth.currentUser === false ? (
+          <div>Verifique seu email</div>
+        ) : (
+          <div>E-mail verificado</div>
+        )}
+        <div>Olá, seja bem vindo. Você está logado</div>
         <div>
-            <h2>Bem vindo</h2>
-            {auth.currentUser===false?<div>Verifique seu email</div>:<div>E-mail verificado</div>}
-          <div>
-              Olá, seja bem vindo. Você está logado
-          </div>
-          <div>User email: {cUser}</div>
-          <button onClick={logout}>Sair</button>
+          User email: <span>{userEmail}</span>
         </div>
-        </>         
-    )
-        
-}     
+        <button onClick={logout}>Sair</button>
+      </div>
+    </>
+  );
+};
 export default Home;
